dc349d373472e08b5ca524c3acb283cf8d09c390
Lishin <lishinvs6541@gmail.com>
2024-09-01 21:51:47 +0530

    A2_Q1: Add comments for better readability

--- Assignment 2/B231045CS_LISHIN_2.asm
+++ Assignment 2/B231045CS_LISHIN_2.asm
@@ -1,34 +1,43 @@
-.globl main
-.data
-msg1: .asciiz "Enter a positive number: "
-msg2: .asciiz "Factorial: "
+.globl main		# declare main as global entry point
 
-.text
-main:
-li $v0,4
-la $a0,msg1
-syscall
+.data			# Section for declaring initialized data
+	one: .word 1
+	msg1: .asciiz "Enter a positive number: "
+	msg2: .asciiz "Factorial: "
 
-li $v0,5
-syscall
-move $t0,$v0
+.text			# Code section
+main:		
+	# print message 1
+	li $v0,4
+	la $a0,msg1
+	syscall
 
-addi $t1,$zero,1
+	# read the positive number
+	li $v0,5
+	syscall
+	move $t0,$v0
+	
+	# initialize the result to one 
+	lw $t1,one
 
 fact:	
-	beq $t0,$zero,exit
-	mul $t1,$t1,$t0
-	sub $t0,$t0,1
-	j fact
+	# factorial loop 
+	beq $t0,$zero,exit			# exit if the number becomes equal to 0
+	mul $t1,$t1,$t0			# multiply the result with earlier value of result 
+	sub $t0,$t0,1				# reduce the number by 1
+	j fact					# jump back to the loop
 	
 exit:
+	# print the factorial message
 	li $v0,4
 	la $a0,msg2
 	syscall 
 	
+	# print the result(factorial)
 	li $v0,1
 	move $a0,$t1
 	syscall 
 	
+	# Exit the program
 	li $v0,10
 	syscall
